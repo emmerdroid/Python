e2ae3186ac3d8bcb9371f6f78fd74a7308415311
emmerdroid <58455108+emmerdroid@users.noreply.github.com>
2021-06-03 10:35:40 -0700

    Update GameTest.py
    added more parts trying to use PyGames internal drawing functions instead of importing imaged

--- GameTest.py
+++ GameTest.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.display import update
 
 #initilization of pygame
 pygame.init()
@@ -16,13 +17,26 @@ pygame.display.set_caption("Space Invaders")
 #player with image
 #playerImage = pygame.image.load(name of file)
 
-#player with internal grraphing
+#player with internal graphing
 color = (255, 175, 0)
 playerX = 500
 playerY = 250
-playerRect = pygame.draw.rect(screen,color, pygame.Rect(30, 30, 50, 50))
+playerRect = pygame.draw.rect(screen,color, pygame.Rect(playerX, playerY, 50, 50))
 
+#bullet set up
+class Bullet():
+    bulletX = playerX + 25
+    bulletY = playerY
+    
+    def __init__(self) -> None:
+        pygame.draw.circle(screen, (180, 50, 20), (bulletX,bulletY), 10)
 
+    def update(self):
+        self.bulletY += 5
+        pass
+
+#clock set up for each frame
+clock = pygame.time.Clock()
 
 # game loop to allow it to keep going
 running = True
@@ -32,5 +46,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     #RGB fill of the screen
-    
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]: playerY -= 3
+    if pressed[pygame.K_DOWN]: playerY += 3
+    if pressed[pygame.K_RIGHT]: playerX += 3
+    if pressed[pygame.K_LEFT]: playerX -= 3
+    if pressed[pygame.K_SPACE]: 
+        Bullet.__init__(Bullet)
+        Bullet.update(Bullet)
+
+
+    pygame.draw.rect(screen,color, pygame.Rect(playerX, playerY, 50, 50))
     pygame.display.flip()
+    clock.tick(60)
